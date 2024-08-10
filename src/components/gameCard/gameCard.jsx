ce8ed6ddae5b5f6f9d147fa656135ad8234df668
Sebastian-Paulluk <sebastianpaulluk@gmail.com>
2024-08-09 21:26:18 -0300

    Guardar el juego o quitarlo de la lista de guardados esconde el panel de info de las cards

--- src/components/gameCard/gameCard.jsx
+++ src/components/gameCard/gameCard.jsx
@@ -38,6 +38,7 @@ export default function GameCard({game, mountingDelay}) {
 
     const handleSaveGameClick = (game) => {
         toggleGameOnSavedList(game);
+        setOpenGameInfoPanel(false)
     };
 
     const handleOpenModal = () => {
